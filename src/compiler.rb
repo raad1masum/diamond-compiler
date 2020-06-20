7a6a38475b940c937cf111553b47de52d8acd0c9
raad1masum <piraadwan@gmail.com>
2020-06-20 12:32:54 -0700

    move everything into compile method

--- src/compiler.rb
+++ src/compiler.rb
@@ -1,15 +1,15 @@
 require 'redcarpet'
 
-md_options = {
-  tables: true,
-  fenced_code_blocks: true,
-  autolink: true,
-  strikethrough: true
-}
-
-processor = Redcarpet::Markdown.new(Redcarpet::Render::HTML, md_options)
-
 def compile (file_name)
+  md_options = {
+    tables: true,
+    fenced_code_blocks: true,
+    autolink: true,
+    strikethrough: true
+  }
+  
+  processor = Redcarpet::Markdown.new(Redcarpet::Render::HTML, md_options)
+
   markdown = File.read(file_name.to_str)
   puts processor.render(markdown)
 end
