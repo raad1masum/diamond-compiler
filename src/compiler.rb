27c8958bee9138baf0c27211d5661ab8b838de38
raad1masum <piraadwan@gmail.com>
2020-06-20 11:05:26 -0700

    make compiler work

--- src/compiler.rb
+++ src/compiler.rb
@@ -2,4 +2,13 @@ require 'redcarpet'
 
 markdown = File.read('README.md')
 
-Redcarpet::Markdown.new(Redcarpet::Render::HTML.new).render(markdown)
+md_options = {
+  tables: true,
+  fenced_code_blocks: true,
+  autolink: true,
+  strikethrough: true
+}
+
+processor = Redcarpet::Markdown.new(Redcarpet::Render::HTML, md_options)
+
+puts processor.render(markdown)
